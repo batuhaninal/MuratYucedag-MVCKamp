1b3a51bc913c886c53249b282369286607e8e1aa
ked ked <np_batu@hotmail.com>
2021-10-15 01:16:20 +0300

    Video 30

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.List();
+        }
+    }
+}
---
+++ MY_MVCProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MY_MVCProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category entity)
+        {
+            //categoryManager.Add(entity);
+            return RedirectToAction("GetList");
+        }
+    }
+}
